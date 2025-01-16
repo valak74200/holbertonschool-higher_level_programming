ec97470043f7bc42304025cc678c2f84cdaad6a0
valak74200 <clmentlv@outlook.fr>
2025-01-16 11:32:29 +0100

    Refactor 3-infinite_add.py to use sys module directly and streamline argument summation

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 
-from sys import argv
-
-
 if __name__ == "__main__":
-    x = 0
-    if len(argv) == 1:
-        print("0")
-    else:
-        for i in range(1, len(argv)):
-            x += int(argv[i])
-        print(x)
+    import sys
+
+
+    result = 0
+    for i in range(1, len(sys.argv)):
+        result += int(sys.argv[i])
+    print(result)
