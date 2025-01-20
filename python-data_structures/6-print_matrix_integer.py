a7831a8d09591dde787c2a9930a5484df1ae6f24
valak74200 <clmentlv@outlook.fr>
2025-01-20 16:49:08 +0100

    Add add_tuple function and main script; enhance comments in print_matrix_integer function

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            if j == len(matrix[i]) - 1:
-                print("{:d}".format(matrix[i][j]), end="")
+def print_matrix_integer(matrix=[[]]): # Définition de la fonction avec une matrice vide comme argument par défaut
+    for i in range(len(matrix)): # Boucle sur les lignes de la matrice
+        for j in range(len(matrix[i])): # Boucle sur les colonnes de chaque ligne
+            if j == len(matrix[i]) - 1: # Vérifie si c'est le dernier élément de la ligne
+                print("{:d}".format(matrix[i][j]), end="") # Affiche l'élément sans espace à la fin
             else:
-                print("{:d}".format(matrix[i][j]), end=" ")
-        print()
+                print("{:d}".format(matrix[i][j]), end=" ") # Affiche l'élément avec un espace à la fin
+        print() # Passe à la ligne suivante après avoir affiché tous les éléments d'une ligne
