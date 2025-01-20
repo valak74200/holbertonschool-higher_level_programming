2c0a293a3804a8565c358c64a3b64483aa6fdf7a
valak74200 <clmentlv@outlook.fr>
2025-01-20 16:58:32 +0100

    Add delete_at function and main script; enhance comments in divisible_by_2 function for clarity

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def divisible_by_2(my_list=[]):
-    new_list = []
-    for i in my_list:
-        if i % 2 == 0:
-            new_list.append(True)
+def divisible_by_2(my_list=[]): # Définition de la fonction avec une liste vide comme argument par défaut
+    new_list = [] # Initialise une nouvelle liste vide pour stocker les résultats
+    for i in my_list: # Parcourt chaque élément de la liste d'entrée
+        if i % 2 == 0: # Vérifie si l'élément est divisible par 2 (pair)
+            new_list.append(True) # Si pair, ajoute True à la nouvelle liste
         else:
-            new_list.append(False)
-    return new_list
+            new_list.append(False) # Si impair, ajoute False à la nouvelle liste
+    return new_list # Retourne la nouvelle liste contenant les résultats
