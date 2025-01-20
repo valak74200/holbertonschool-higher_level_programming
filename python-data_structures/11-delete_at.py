bf6dbfab4850fd3502babaedcdb1ad75509f1dc1
valak74200 <clmentlv@outlook.fr>
2025-01-20 17:00:32 +0100

    Enhance delete_at function with detailed comments; add switch.py script to demonstrate variable swapping

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
-    del my_list[idx]
-    return my_list
+def delete_at(my_list=[], idx=0): # Définition de la fonction avec une liste vide et un index 0 comme arguments par défaut
+    if idx < 0 or idx >= len(my_list): # Vérifie si l'index est négatif ou hors de la plage de la liste
+        return my_list # Si l'index est invalide, retourne la liste sans modification
+    del my_list[idx] # Supprime l'élément à l'index spécifié de la liste
+    return my_list # Retourne la liste modifiée
