0b52d6daa05cc3d1f1b1be3ebd4ac94dfb9b6a1a
valak74200 <clmentlv@outlook.fr>
2025-01-20 17:12:04 +0100

    Remove unused main script files from python-data_structures and python-if_else_loops_functions

--- python-data_structures/12-switch.py
+++ python-data_structures/12-switch.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-a = 89
-b = 10
-a, b = b, a
-print("a={:d} - b={:d}".format(a, b))
+a = 89 # Initialise la variable a avec la valeur 89
+b = 10 # Initialise la variable b avec la valeur 10
+a, b = b, a # Échange les valeurs de a et b : a devient 10, b devient 89
+print("a={:d} - b={:d}".format(a, b)) # Affiche les nouvelles valeurs de a et b
