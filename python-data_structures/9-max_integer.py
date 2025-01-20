ec598d339564864a631bb78a639766b51e6c23ab
valak74200 <clmentlv@outlook.fr>
2025-01-20 16:56:38 +0100

    Add divisible_by_2 function and main script; enhance comments in max_integer function for clarity

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
-    if my_list:
-        max_int = my_list[0]
-        for i in my_list:
-            if i > max_int:
-                max_int = i
-        return max_int
-    return None
+def max_integer(my_list=[]): # Définition de la fonction avec une liste vide comme argument par défaut
+    if my_list: # Vérifie si la liste n'est pas vide
+        max_int = my_list[0] # Initialise max_int avec le premier élément de la liste
+        for i in my_list: # Parcourt chaque élément de la liste
+            if i > max_int: # Compare l'élément actuel avec max_int
+                max_int = i # Si l'élément est plus grand, met à jour max_int
+        return max_int # Retourne la valeur maximale trouvée
+    return None # Si la liste est vide, retourne None
