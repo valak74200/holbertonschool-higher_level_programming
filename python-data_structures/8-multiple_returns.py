33920a780ffd5afafe04b5c8590cd3ae6f7ea182
valak74200 <clmentlv@outlook.fr>
2025-01-20 16:54:07 +0100

    Add max_integer function and main script; enhance comments in multiple_returns function for clarity

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    a = len(sentence)
-    if a == 0:
-        b = None
+def multiple_returns(sentence):  # Définition de la fonction avec un paramètre : sentence
+    a = len(sentence) # Calcule la longueur de la phrase et la stocke dans la variable a
+    if a == 0: # Vérifie si la longueur de la phrase est 0
+        b = None # Si la phrase est vide, b est défini comme None
     else:
-        b = sentence[0]
-    return (a, b)
+        b = sentence[0] # Sinon, b est défini comme le premier caractère de la phrase
+    return (a, b) # Retourne un tuple contenant la longueur et le premier caractère (ou None)
