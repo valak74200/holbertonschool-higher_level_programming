dfb68f4c905d84ae364a0622a577e730b7e64c9f
valak74200 <clmentlv@outlook.fr>
2025-01-20 16:16:42 +0100

    Enhance comments in no_c function for better clarity and understanding

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def no_c(my_string):
-    new_string = ''
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_string += char
-    return new_string
+def no_c(my_string): # Définition de la fonction avec un paramètre : my_string
+    new_string = '' # Initialise une nouvelle chaîne vide pour stocker les caractères filtrés
+    for char in my_string: # Boucle pour parcourir chaque caractère dans my_string
+        if char != 'c' and char != 'C': # Vérifie si le caractère n'est pas 'c' ou 'C'
+            new_string += char # Ajoute le caractère à new_string s'il ne s'agit pas de 'c' ou 'C'
+    return new_string # Retourne la nouvelle chaîne sans les caractères 'c' et 'C'
