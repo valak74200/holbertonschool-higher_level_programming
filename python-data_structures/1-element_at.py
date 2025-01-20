d906df98ab9b9acf23882e37f7f9f59d59034fab
valak74200 <clmentlv@outlook.fr>
2025-01-20 15:56:41 +0100

    Enhance comments in element_at function for better clarity and understanding

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    a = len(my_list)
-    if idx < 0:
-        return None
-    if idx >= a:
-        return None
-    return my_list[idx]
+def element_at(my_list, idx): # Définition de la fonction avec deux paramètres : my_list et idx
+    a = len(my_list) # Calcule la longueur de my_list et stocke le résultat dans la variable a
+    if idx < 0: # Vérifie si l'index est négatif
+        return None # Si l'index est négatif, retourne None
+    if idx >= a: # Vérifie si l'index est supérieur ou égal à la longueur de la liste
+        return None # Si l'index est hors limites, retourne None
+    return my_list[idx] # Retourne l'élément de my_list à l'index idx
