c46b5e4c045d0e7114b6fd87714320411366cd6a
valak74200 <clmentlv@outlook.fr>
2025-01-20 16:15:32 +0100

    Add no_c function to remove 'c' and 'C' from strings; create main script for testing

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    a = len(my_list)
-    b = my_list.copy()
-    if idx < 0:
-        return b
-    if idx >= a:
-        return b
-    b[idx] = element
-    return b
+def new_in_list(my_list, idx, element): # Définition de la fonction avec trois paramètres : my_list, idx et element
+    a = len(my_list) # Calcule la longueur de my_list et stocke le résultat dans la variable a
+    b = my_list.copy() # Crée une copie de my_list et la stocke dans la variable b
+    if idx < 0: # Vérifie si l'index est négatif
+        return b # Si l'index est négatif, retourne la copie de la liste sans modification
+    if idx >= a: # Vérifie si l'index est supérieur ou égal à la longueur de la liste
+        return b # Si l'index est hors limites, retourne la copie de la liste sans modification
+    b[idx] = element # Remplace l'élément à l'index idx dans la copie de la liste par le nouvel élément 
+    return b # Retourne la copie modifiée de la liste
