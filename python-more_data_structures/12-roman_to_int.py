6dd8bc078bc1fa6e8cc5f00692f198d9afbd7646
valak74200 <clmentlv@outlook.fr>
2025-01-21 18:11:42 +0100

    Refactor roman_to_int.py to simplify code and improve readability

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,24 +1,13 @@
 #!/usr/bin/python3
-
 def roman_to_int(roman_string):
-    # Cette fonction convertit un nombre romain en entier
-
-    # Vérification que l'entrée est une chaîne de caractères non nulle
-    if not isinstance(roman_string, str) or roman_string is None:
+    if not roman_string or not isinstance(roman_string, str):
         return 0
-    
-    # Dictionnaire des valeurs des chiffres romains
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-
-    num = 0  # Initialisation du résultat
+    r_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+              'C': 100, 'D': 500, 'M': 1000}
+    num = 0
     for i in range(len(roman_string)):
-        # Vérification des cas spéciaux (comme IV, IX, XL, etc.)
-        if i > 0 and roman_dict[roman_string[i]] > roman_dict[roman_string[i - 1]]:
-            # Si le chiffre actuel est plus grand que le précédent, on soustrait deux fois
-            # la valeur du chiffre précédent (car on l'a déjà ajouté une fois)
-            num += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i - 1]]
+        if i > 0 and r_dict[roman_string[i]] > r_dict[roman_string[i - 1]]:
+            num += r_dict[roman_string[i]] - 2 * r_dict[roman_string[i - 1]]
         else:
-            # Sinon, on ajoute simplement la valeur du chiffre actuel
-            num += roman_dict[roman_string[i]]
-    
-    return num  # Retourne le résultat final
+            num += r_dict[roman_string[i]]
+    return num
