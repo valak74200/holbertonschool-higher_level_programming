f6e857e0935791b68a2786345ad3ab2ee0347d34
valak74200 <clmentlv@outlook.fr>
2025-01-21 18:09:44 +0100

    Refactor 5-variable_load.py to simplify output formatting and remove unnecessary comments

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-# Cette ligne est un "shebang" qui indique que ce script doit être exécuté avec Python3
-
-# Vérifie si le script est exécuté directement (non importé comme module)
 if __name__ == "__main__":
-   # Importe la variable 'a' depuis le module variable_load_5
-   from variable_load_5 import a
-
-   # Affiche la valeur de la variable 'a' en utilisant un f-string
-   # f'{a}' est une syntaxe de formatage qui permet d'insérer directement la valeur de 'a'
-   print(f'{a}')
+    from variable_load_5 import a
+    print("{}".format(a))
