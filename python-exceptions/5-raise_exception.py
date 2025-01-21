0b6f743f0d0c5c3e28343262affed0691c79c9fe
valak74200 <clmentlv@outlook.fr>
2025-01-21 17:49:55 +0100

    Enhance raise_exception function with comments for clarity

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
+
 def raise_exception():
+    # Définition de la fonction raise_exception sans paramètres
+    
     raise TypeError
+    # Lève une exception de type TypeError sans message spécifique
