4e1bf4a2a2f3d7f959ab9b7ccc61fc620b8b528b
valak74200 <clmentlv@outlook.fr>
2025-01-21 17:07:09 +0100

    Enhance common_elements function with detailed comments for clarity

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+
 def common_elements(set_1, set_2):
+    # Cette fonction prend deux ensembles en entrée et retourne un nouvel ensemble
+    # contenant les éléments communs aux deux ensembles
+
+    # Utilisation de l'opérateur & pour calculer l'intersection des deux ensembles
     return set_1 & set_2
+    # L'opérateur & entre deux ensembles retourne un nouvel ensemble contenant
+    # uniquement les éléments présents dans les deux ensembles d'origine
