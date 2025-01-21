3f18bee0adea9d07b84ea2818440096a73bc229e
valak74200 <clmentlv@outlook.fr>
2025-01-21 17:05:16 +0100

    Enhance uniq_add function with detailed docstring and comments for clarity

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+
 def uniq_add(my_list=[]):
+    # Cette fonction prend une liste en entrée et retourne la somme de tous ses éléments uniques
+
+    # Utilisation de set() pour obtenir les éléments uniques, puis sum() pour les additionner
     return sum(set(my_list))
+    # 1. set(my_list) : Crée un ensemble (set) à partir de my_list, éliminant ainsi les doublons
+    # 2. sum(...) : Calcule la somme de tous les éléments de l'ensemble
+    # Le résultat est la somme de tous les éléments uniques de la liste
