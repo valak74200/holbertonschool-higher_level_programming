cabd378e88a4816e6c68c590da571a020b7a154c
valak74200 <clmentlv@outlook.fr>
2025-01-21 18:10:47 +0100

    Add comments to 5-variable_load.py for improved code clarity

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+# Cette ligne indique que le script doit être exécuté avec l'interpréteur Python 3
+
 if __name__ == "__main__":
+    # Cette condition vérifie si le script est exécuté directement (et non importé comme module)
+
     from variable_load_5 import a
+    # Importe la variable 'a' depuis le fichier 'variable_load_5.py'
+
     print("{}".format(a))
+    # Affiche la valeur de la variable 'a' importée
