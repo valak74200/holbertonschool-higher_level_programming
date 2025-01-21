943728857427c21fdfc6e98d899742816b5897e2
valak74200 <clmentlv@outlook.fr>
2025-01-21 17:14:31 +0100

    Enhance update_dictionary function with detailed comments for clarity

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+
 def update_dictionary(a_dictionary, key, value):
+    # Cette fonction met à jour un dictionnaire avec une nouvelle paire clé-valeur
+    # ou met à jour la valeur si la clé existe déjà
+
+    # Mise à jour ou ajout de la paire clé-valeur dans le dictionnaire
     a_dictionary[key] = value
+    # Si la clé existe déjà, sa valeur est mise à jour
+    # Si la clé n'existe pas, une nouvelle paire clé-valeur est ajoutée
+
+    # Retourne le dictionnaire mis à jour
     return a_dictionary
