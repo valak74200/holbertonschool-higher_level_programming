955835b4f1802c68b9266a61fc531f28b2540d6b
valak74200 <clmentlv@outlook.fr>
2025-01-21 17:16:34 +0100

    Enhance simple_delete function with comments for better understanding

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+
 def simple_delete(a_dictionary, key=""):
+    # Cette fonction supprime une paire clé-valeur d'un dictionnaire si la clé existe
+
+    # Vérifie si la clé existe dans le dictionnaire
     if key in a_dictionary:
+        # Si la clé existe, on la supprime du dictionnaire
         del a_dictionary[key]
+    
+    # Retourne le dictionnaire (modifié ou non)
     return a_dictionary
