d0a57f7a4045d9eb68d6a0f4a1e38434d801941e
valak74200 <clmentlv@outlook.fr>
2025-01-21 17:42:57 +0100

    Enhance safe_print_integer function with detailed comments and exception handling

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
+
 def safe_print_integer(value):
+    # Définition de la fonction qui prend un paramètre 'value'
+    
     try:
+        # Début du bloc try pour gérer les exceptions potentielles
+        
         print("{:d}".format(value))
+        # Tente d'imprimer 'value' formaté comme un entier
+        # {:d} force le formatage en entier
+        
         return True
+        # Si l'impression réussit, retourne True
+    
     except Exception:
+        # Capture toute exception qui pourrait se produire
+        
         return False
+        # Si une exception se produit, retourne False
