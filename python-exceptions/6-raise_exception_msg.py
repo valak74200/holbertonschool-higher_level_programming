da8be386ed4fd0af59557444a47d0556a4c66f43
valak74200 <clmentlv@outlook.fr>
2025-01-21 17:51:14 +0100

    Enhance raise_exception_msg function with comments for clarity

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+
 def raise_exception_msg(message=""):
+    # Définition de la fonction raise_exception_msg avec un paramètre 'message' optionnel
+    # Si aucun message n'est fourni, il sera une chaîne vide par défaut
+    
     raise NameError(message)
+    # Lève une exception de type NameError avec le message spécifié
