f826b4a6dbb30bfdd8c7e9383dbb1699b56714b3
valak74200 <clmentlv@outlook.fr>
2025-01-21 17:09:16 +0100

    Enhance only_diff_elements function with detailed comments for clarity

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+
 def only_diff_elements(set_1, set_2):
+    # Cette fonction prend deux ensembles en entrée et retourne un nouvel ensemble
+    # contenant les éléments qui sont présents dans l'un des ensembles mais pas dans les deux
+
+    # Utilisation de l'opérateur ^ pour calculer la différence symétrique entre les deux ensembles
     return set_1 ^ set_2
+    # L'opérateur ^ (différence symétrique) entre deux ensembles retourne un nouvel ensemble
+    # contenant tous les éléments qui sont dans set_1 ou set_2, mais pas dans les deux à la fois.
