cafd99002597977843b2a39adca0b1e2cec994ff
valak74200 <clmentlv@outlook.fr>
2025-01-26 01:12:53 +0100

    Translate say_my_name function and its docstring to French for localization

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-"""Function that prints My name is <first name> <last name>."""
+"""Fonction qui imprime Mon nom est <prénom> <nom>."""
 
 
 def say_my_name(first_name, last_name=""):
-    """Print My name is <first name> <last name>."""
+    """Imprime Mon nom est <prénom> <nom>."""
+    # Vérifie si first_name est une chaîne de caractères
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
+    
+    # Vérifie si last_name est une chaîne de caractères
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
+    
+    # Imprime le nom complet
     print("My name is {} {}".format(first_name, last_name))
