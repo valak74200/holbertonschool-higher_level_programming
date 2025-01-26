5377fe88f08de70954fd8482acfc639d1fc39db0
valak74200 <clmentlv@outlook.fr>
2025-01-26 01:17:33 +0100

    Translate text_indentation function and its docstring to French for localization

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,30 +1,41 @@
 #!/usr/bin/python3
-"""This module contains a function
-    that prints a text with 2 new lines after.
+"""Ce module contient une fonction
+    qui imprime un texte avec 2 nouvelles lignes après.
 """
 
 
 def text_indentation(text):
-    """Print a text with 2 new lines after.
+    """Imprime un texte avec 2 nouvelles lignes après.
 
     Args:
-        text (str): The text to be printed
+        text (str): Le texte à imprimer
 
     Raises:
-        TypeError: If text is not a string
+        TypeError: Si le texte n'est pas une chaîne de caractères
 
     Returns:
-        _type_: The text with 2 new lines after
+        str: Le texte avec 2 nouvelles lignes après
     """
+    # Vérifie si le texte est une chaîne de caractères
     if type(text) is not str:
         raise TypeError("text must be a string")
+    
+    # Initialise une nouvelle chaîne de caractères vide
     new_text = ""
+    
+    # Parcourt chaque caractère du texte
     for i in text:
+        # Ignore les espaces au début du texte
         if i == " " and len(new_text) == 0:
             continue
+        # Ignore les espaces après une nouvelle ligne
         elif i == " " and new_text[-1] in "\n":
             continue
+        # Ajoute le caractère au nouveau texte
         new_text += i
+        # Ajoute deux nouvelles lignes après '.', '?', ':' ou '!'
         if i in ".?:!":
             new_text += "\n\n"
+    
+    # Imprime le nouveau texte sans ajouter de nouvelle ligne à la fin
     print(new_text, end="")
